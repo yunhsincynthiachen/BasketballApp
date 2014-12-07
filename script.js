155f14578def8889a565686520ed9c45a31cf5bf
Yun-Hsin Cynthia Chen <yun-hsin.chen@students.olin.edu>
2014-12-07 16:47:58 -0500

    Fixed data update

--- script.js
+++ script.js
@@ -1,41 +1,43 @@
-var cynthia_wins = 0;
-var adit_wins = 0;
 var totals = {
-	"adit":{
-		teams:['Houston','Chicago','Dallas','Cleveland','Washington','Miami','Oklahoma City'],
+	"Adit":{
+		teams: ['Chicago','Cleveland','Dallas','Houston','Miami','Oklahoma City','Washington'],
 		total_wins:0,
-		total_losses:0
+		dict_of_teams: {},
 	},
-	"cynthia":{
-		teams:['Golden State','Memphis','Toronto','Portland','San Antonio','L.A. Clippers','Atlanta'],
+	"Cynthia":{
+		teams: ['Atlanta','Golden State', 'L.A. Clippers', 'Memphis','Portland','San Antonio','Toronto'],
 		total_wins:0,
-		total_losses:0
+		dict_of_teams:{},
+	},
+	"Both":{
+		total_wins: [],
 	}
 }
+
 function kimonoCallback(data) {
 // do something with the data
 // please make sure the scope of this function is global
 // alert('abc');
-	//console.log(data);
-	var adit_teams = ['Houston','Chicago','Dallas','Cleveland','Washington','Miami','Oklahoma City'];
-	var cynthia_teams = ['Golden State','Memphis','Toronto','Portland','San Antonio','L.A. Clippers','Atlanta'];
-	var dictof_aditteams = {};
-	var dictof_cynthiateams = {};
-	var alldata = data.results['NBA Parse']
+	var alldata = data.results['NBA Parse'];
+	//next part makes dictionary in the totals variable into variable
+	var dictof_aditteams = totals['Adit']['dict_of_teams'];
+	var dictof_cynthiateams = totals['Cynthia']['dict_of_teams'];
+	//makes the total wins of both to be a list of the wins
+	var total_wins_both = totals['Both']['total_wins'];
 	console.log(data)
 
 	//To make dictionaries of the team names and number of wins of each person
-	for (j = 0; j<adit_teams.length; j++){
+	for (j = 0; j<totals['Adit']['teams'].length; j++){
 		for (i = 0; i<alldata['length']; i++) {
-			if (adit_teams[j] == alldata[i]['Team Names']['text']){
+			if (totals['Adit']['teams'][j] == alldata[i]['Team Names']['text']){
 				dictof_aditteams[alldata[i]['Team Names']['text']] = alldata[i].Wins;
 			}
 		}
 	};
 	
-	for (j = 0; j<cynthia_teams.length; j++){
+	for (j = 0; j<totals['Cynthia']['teams'].length; j++){
 		for (i = 0; i<alldata['length']; i++) {
-			if (cynthia_teams[j] == alldata[i]['Team Names']['text']){
+			if (totals['Cynthia']['teams'][j] == alldata[i]['Team Names']['text']){
 				dictof_cynthiateams[alldata[i]['Team Names']['text']] = alldata[i].Wins;
 			}
 		}
@@ -43,11 +45,11 @@ function kimonoCallback(data) {
 
 	//To total up all of the wins for each person
 	for (var key in dictof_aditteams){
-		adit_wins += +dictof_aditteams[key];
+		totals['Adit']['total_wins'] += +dictof_aditteams[key];
 	}
 
 	for (var key in dictof_cynthiateams){
-		cynthia_wins += +dictof_cynthiateams[key];
+		totals['Cynthia']['total_wins'] += +dictof_cynthiateams[key];
 	}
 
 	//alert(adit_wins+', '+cynthia_wins);
@@ -65,10 +67,15 @@ $.ajax({
 $(document).ready(function(){
 	$('.Button').click(function(){
 		var person = $(this).attr('data-person');
-		alert('You clicked on '+person);
+		//alert('You clicked on '+person);
 		console.log(totals[person]);
-			$('#Right').html('')
-			$('#Right').html('<p>Adit: '+ adit_wins+'</p>' + '<p>Cynthia: '+ cynthia_wins+'</p>')
+		if (person == "Both") {
+			$('#Right').html('<p>Adit: '+ totals['Adit']['total_wins']+'</p>' + '<p>Cynthia: '+ totals['Cynthia']['total_wins']+'</p>')
+		}
+		else {
+			$('#Right').html(person + ': ' + totals[person]['total_wins'])
+		}
+			//$('#Right').html('<p>Adit: '+ adit_wins+'</p>' + '<p>Cynthia: '+ cynthia_wins+'</p>')
 	})
 
 });
